08b0d7cc3284ef4e47ad311211934c67ad50c65b
Thiago Tavares <Thiagoff2019@hotmail.com>
2024-04-19 15:25:29 -0300

    dart test

---
+++ bin/untitled1.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+import 'package:untitled1/untitled1.dart' as untitled1;
+
+void main(List<String> arguments) {
+  print("Hello world: ${untitled1.calculate()}!");
+  var valor = stdin.readLineSync();
+  print(valor);
+
+}
